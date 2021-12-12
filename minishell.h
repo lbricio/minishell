188420b32a75a5dc11bacacd8ed0e23368adab4d
lbricio <kirowhisky@gmail.com>
2021-12-12 15:12:32 -0300

    237 erros inexistentes resolvidos

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: lbricio- <lbricio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 15:04:50 by felipe            #+#    #+#             */
-/*   Updated: 2021/12/12 14:57:22 by lbricio-         ###   ########.fr       */
+/*   Updated: 2021/12/12 15:11:20 by lbricio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define MINISHELL_H
 
 # include <fcntl.h>
-# include "gnl/get_next_line.h"
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
@@ -102,5 +101,10 @@ int		execute(t_cmds *cmds, char **envp, S_SIG **act);
 int		check_quotation(char *line);
 int		ft_atoi(const char *str);
 int		open_file(char *argv, int i);
+
+int		ft_strlen(char *str);
+char	*ft_strdup(const char *s);
+char	*ft_concat(char **dest, char *src);
+char	*ft_strchr(const char *s, int c);
 
 #endif
--- utils/utils.c
+++ utils/utils.c
@@ -6,11 +6,60 @@
 /*   By: lbricio- <lbricio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 21:17:18 by felipe            #+#    #+#             */
-/*   Updated: 2021/12/11 21:30:12 by lbricio-         ###   ########.fr       */
+/*   Updated: 2021/12/12 15:07:42 by lbricio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	dup = malloc(i + 1);
+	if (!dup)
+		return (0);
+	i = 0;
+	while (s[i] != 0)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = s[i];
+	return (dup);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != 0)
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	if (c >= 256)
+		c = c - 256;
+	i = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)&s[i]);
+	return (0);
+}
 
 void	lstadd_back(t_vars **lst, t_vars *new)
 {
@@ -189,4 +238,32 @@ char	*cmds_to_string(t_cmds *cmds)
 		free(temp);
 	}
 	return (line);
+}
+
+char	*ft_concat(char **dest, char *src)
+{
+	char	*new;
+	int		i;
+	int		len_dest;
+	int		len_src;
+
+	len_dest = ft_strlen((*dest));
+	len_src = ft_strlen(src);
+	new = malloc(len_dest + len_src + 1);
+	if (!new)
+	{
+		free((*dest));
+		return (0);
+	}
+	i = -1;
+	while (++i < len_dest)
+		new[i] = (*dest)[i];
+	while (i < len_dest + len_src)
+	{
+		new[i] = src[i - len_dest];
+		i++;
+	}
+	new[i] = 0;
+	free((*dest));
+	return (new);
 }
